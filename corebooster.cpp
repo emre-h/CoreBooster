fa68bcb69b6ad5b33fc4663295ba34d583b70f81
Emre Harbutoğlu <coder44278@gmail.com>
2017-01-18 17:26:50 +0200

    Fixes & Stabilization

--- corebooster.cpp
+++ corebooster.cpp
@@ -3,48 +3,23 @@
 using namespace std;
 
 void ultraMode(){
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
+  system("sudo chmod 777 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
+  system("sudo chmod 777 /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
+  system("sudo echo ondemand > /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
   system("sudo echo 11 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
 }
 
 void normalMode(){
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-  system("sudo chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
-  system("sudo echo 95 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
+  system("sudo chmod 777 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
+  system("sudo chmod 777 /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
+  system("sudo echo powersave > /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
+ // system("sudo echo 11 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
+ // system("sudo echo 95 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
 }
 #define ULTRA ultraMode();
 #define NORMAL normalMode();
 int main(){
-  cout << "Welcome to the CoreBooster v1.0" << '\n';
+  cout << "Welcome to the CoreBooster v1.0.1 by Emre Harbutoğlu" << '\n';
   if (geteuid() != 0) {
   cout << "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n";
   cout << "PLEASE GRANT ROOT PRIVILIGES AND RESTART THE COREBOOSTER\n";
