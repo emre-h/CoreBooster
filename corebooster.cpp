ee7f06a9a7435e2b1a790b3efde7f3ad23634f2b
Emre Harbutoğlu <coder44278@gmail.com>
2017-01-18 19:06:02 +0200

    Update corebooster.cpp

--- corebooster.cpp
+++ corebooster.cpp
@@ -5,7 +5,10 @@ using namespace std;
 void ultraMode(){
   system("sudo chmod 777 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
   system("sudo chmod 777 /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
-  system("sudo echo ondemand > /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
+  system("sudo echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
+   system("sudo echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
+   system("sudo echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
+   system("sudo echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
   system("sudo echo 11 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
 }
 
