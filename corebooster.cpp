deedd4ae4dceee77b8e3053256e09e46279a7630
Emre Harbutoğlu <emre.harbutoglu@hemenis.com>
2020-06-09 18:35:21 +0300

    fetching some params about system

--- corebooster.cpp
+++ corebooster.cpp
@@ -1,8 +1,9 @@
 /*
 * UNDER DEVELOPMENT
+* Author: Emre Harbutoğlu
 */
 
-
+#include <fstream>
 #include <iostream>
 #include <unistd.h>
 #include <thread>
@@ -11,40 +12,52 @@
 using namespace std;
 
 struct cpu_params {
-  const char *governors;
+  string governors;
   int cores;
 };
 
 cpu_params params;
 
-const string cpuPath = "/sys/devices/system/cpu/cpu";
-const string govPath = "/cpufreq/scaling_governor";
+const string cpu_path = "/sys/devices/system/cpu/cpu";
+const string gov_path = "/cpufreq/scaling_governor";
 
-void coreSetter(string kernelParameterPath) {
+void core_setter(string kernel_parameter_path) {
   
   string command = "";
 
   for (int i = 0; i < params.cores; i++) {
-    string index = cpuPath;
-    index += to_string(i);
-    index += kernelParameterPath;
-    index += i == params.cores - 1 ? "" : " && ";
-
-    command.append(index);
-
-    cout << index << "\n";
+    command.append(cpu_path + to_string(i) + kernel_parameter_path + (i == params.cores - 1 ? "" : " && "));
   }
 
   //system(command.c_str());
 }
 
-int main() {
-
+void set_cpu_params() {
   params.cores = sysconf(_SC_NPROCESSORS_ONLN);
 
-  coreSetter(govPath);
+  ifstream file(cpu_path + to_string(0) + "/cpufreq/scaling_available_governors");
+
+  if (file.is_open()) {
+    getline(file, params.governors);
+
+    file.close();
+  }
+}
+
+bool is_governor_exist(string gov) {
+  return params.governors.find(gov) != string::npos;
+}
+
+void mode_setter(int mode) {
+
+}
+
+int main() {  
+  core_setter(gov_path);
+  set_cpu_params();
 
 
+  cout << is_governor_exist("performance");
 
   return 0;
 }
