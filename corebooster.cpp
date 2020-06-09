617a91747e4958bff3b6e3dd1dc94d946bca83a7
Emre Harbutoğlu <emre.harbutoglu@hemenis.com>
2020-06-09 18:10:15 +0300

    under development

--- corebooster.cpp
+++ corebooster.cpp
@@ -1,83 +1,50 @@
-#include<iostream>
+/*
+* IN PROGRESS
+*/
+
+
+#include <iostream>
 #include <unistd.h>
+#include <thread>
+#include <string> 
+
 using namespace std;
-void setPerms(){
-system("sudo chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu8/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu9/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu10/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu11/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu12/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu13/cpufreq/scaling_governor");
-system("sudo chmod 777 /sys/devices/system/cpu/cpu14/cpufreq/scaling_governor");
-}
-void ultraMode(){
-setPerms();
-system("sudo chmod 777 /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu8/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu9/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu10/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu11/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu12/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu13/cpufreq/scaling_governor");
-system("sudo echo ondemand > /sys/devices/system/cpu/cpu14/cpufreq/scaling_governor");
-system("sudo echo 11 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold");
+
+struct cpu_params {
+  const char *governors;
+  int cores;
+};
+
+cpu_params params;
+
+const string cpuPath = "/sys/devices/system/cpu/cpu";
+const string govPath = "/cpufreq/scaling_governor";
+
+void coreSetter(string kernelParameterPath) {
+  
+  string command = "";
+
+  for (int i = 0; i < params.cores; i++) {
+    string index = cpuPath;
+    index += to_string(i);
+    index += kernelParameterPath;
+    index += i == params.cores - 1 ? "" : " && ";
+
+    command.append(index);
+
+    cout << index << "\n";
+  }
+
+  //system(command.c_str());
 }
 
-void normalMode(){
-setPerms();
-system("sudo echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu8/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu9/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu10/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu11/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu12/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu13/cpufreq/scaling_governor");
-system("sudo echo powersave > /sys/devices/system/cpu/cpu14/cpufreq/scaling_governor");
-}
-#define ULTRA ultraMode();
-#define NORMAL normalMode();
-int main(){
-  cout << "Welcome to the CoreBooster v1.0.1 by metahex (Emre Harbutoğlu)" << '\n';
-  if (geteuid() != 0) {
-  cout << "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n";
-  cout << "PLEASE GRANT ROOT PRIVILIGES AND RESTART THE COREBOOSTER\n";
-  }else {
-  cout << "Modes:" << '\n';
-  usleep(1000000/2);
-  cout << "Ultra :(0)" << '\n';
-  usleep(1000000/2);
-  cout << "Normal :(1)" << '\n';
-string abc;
-cin >> abc;
-if (abc=="0") {
-ULTRA
-}
-if (abc=="1") {
-NORMAL
-}
-}
+int main() {
+
+  params.cores = sysconf(_SC_NPROCESSORS_ONLN);
+
+  coreSetter(govPath);
+
+
+
   return 0;
 }
